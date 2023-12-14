6b2bf259182c5fefacf0335e01fbbf9b099d4890
HEADCUBE <omarkhalfi2010@gmail.com>
2023-12-14 21:52:19 +0000

    haha

---
+++ src/test/java/com/khalfi/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalfi.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
